2c489dff391a4752d9de6c8843a8d6da56048c01
Lucas Otoni <lotoni.vivo@gmail.com>
2023-04-21 17:56:31 -0300

    fix: format code

--- src/project/Principal.java
+++ src/project/Principal.java
@@ -12,117 +12,117 @@ import java.util.Collections;
 import java.util.Comparator;
 
 public class Principal {
-  public static void main(String[] args) {
-    List<Funcionario> funcionarios = new ArrayList<>();
-    final BigDecimal minimumWage = BigDecimal.valueOf(1212.00);
-    Funcionario maria = new Funcionario("Maria", 2000, 10, 18, 2009.44, roles.OPERADOR.toString());
-    Funcionario joao = new Funcionario("João", 1990, 5, 12, 2284.38, roles.OPERADOR.toString());
+    public static void main(String[] args) {
+        List<Funcionario> funcionarios = new ArrayList<>();
+        final BigDecimal minimumWage = BigDecimal.valueOf(1212.00);
+        Funcionario maria = new Funcionario("Maria", 2000, 10, 18, 2009.44, roles.OPERADOR.toString());
+        Funcionario joao = new Funcionario("João", 1990, 5, 12, 2284.38, roles.OPERADOR.toString());
 
-    funcionarios.add(maria);
-    funcionarios.add(joao);
-    funcionarios.add(new Funcionario("Caio", 1961, 5, 2, 9836.14, roles.COORDENADOR.toString()));
-    funcionarios.add(new Funcionario("Miguel", 1988, 10, 14, 19119.88, roles.DIRETOR.toString()));
-    funcionarios
-        .add(new Funcionario("Alice", 1995, 1, 5, 2234.68, roles.RECEPCIONISTA.toString()));
-    funcionarios.add(new Funcionario("Heitor", 1999, 11, 19, 1582.72, roles.OPERADOR.toString()));
-    funcionarios.add(new Funcionario("Arthur", 1993, 3, 31, 4071.84, roles.CONTADOR.toString()));
-    funcionarios.add(new Funcionario("Laura", 1994, 7, 8, 3017.45, roles.GERENTE.toString()));
-    funcionarios
-        .add(new Funcionario("Heloísa", 2003, 5, 24, 1606.85, roles.ELETRICISTA.toString()));
-    funcionarios.add(new Funcionario("Helena", 1996, 9, 2, 2799.93, roles.GERENTE.toString()));
+        funcionarios.add(maria);
+        funcionarios.add(joao);
+        funcionarios.add(new Funcionario("Caio", 1961, 5, 2, 9836.14, roles.COORDENADOR.toString()));
+        funcionarios.add(new Funcionario("Miguel", 1988, 10, 14, 19119.88, roles.DIRETOR.toString()));
+        funcionarios
+                .add(new Funcionario("Alice", 1995, 1, 5, 2234.68, roles.RECEPCIONISTA.toString()));
+        funcionarios.add(new Funcionario("Heitor", 1999, 11, 19, 1582.72, roles.OPERADOR.toString()));
+        funcionarios.add(new Funcionario("Arthur", 1993, 3, 31, 4071.84, roles.CONTADOR.toString()));
+        funcionarios.add(new Funcionario("Laura", 1994, 7, 8, 3017.45, roles.GERENTE.toString()));
+        funcionarios
+                .add(new Funcionario("Heloísa", 2003, 5, 24, 1606.85, roles.ELETRICISTA.toString()));
+        funcionarios.add(new Funcionario("Helena", 1996, 9, 2, 2799.93, roles.GERENTE.toString()));
 
-    System.out.println(
-        "3.1");
-    System.out.println(funcionarios + "\n");
+        System.out.println(
+                "3.1");
+        System.out.println(funcionarios + "\n");
 
-    removeEmployee(funcionarios, "João");
-    System.out.println();
+        removeEmployee(funcionarios, "João");
+        System.out.println();
 
-    printEmployees(funcionarios);
-    System.out.println();
+        printEmployees(funcionarios);
+        System.out.println();
 
-    System.out.println(
-        "3.4");
-    for (Funcionario funcionario : funcionarios) {
-      funcionario.raiseSalary();
-      System.out.println(funcionario);
+        System.out.println(
+                "3.4");
+        for (Funcionario funcionario : funcionarios) {
+            funcionario.raiseSalary();
+            System.out.println(funcionario);
+        }
+
+        System.out.println();
+
+        // 3.5
+        Map<String, List<Funcionario>> sortByRole = funcionarios.stream()
+                .collect(Collectors.groupingBy(Funcionario::getFuncao));
+
+        System.out.println();
+
+        System.out.println("3.6");
+        System.out.println(sortByRole.entrySet());
+
+        System.out.println();
+
+        System.out.println("3.8");
+        System.out
+                .println(
+                        funcionarios.stream()
+                                .filter(s -> s.getDataNascimento().getMonth().getValue() == 10
+                                        || s.getDataNascimento().getMonth().getValue() == 12)
+                                .collect(Collectors.toList()));
+
+        System.out.println();
+
+        System.out.println(
+                "3.9");
+
+        Comparator<Funcionario> comparator = (func1, func2) -> func1.getDataNascimento()
+                .compareTo(func2.getDataNascimento());
+
+        Collections.sort(funcionarios, comparator);
+
+        Funcionario oldest = funcionarios.get(0);
+        System.out
+                .println("Nome: " + oldest.getNome() + ", Idade: " + oldest.getAge() + " anos.");
+
+        System.out.println();
+
+        List<Funcionario> orderedList = funcionarios.stream()
+                .sorted((arg0, arg1) -> arg0.getNome().compareTo(arg1.getNome()))
+                .collect(Collectors.toList());
+
+        System.out.println("3.10");
+        printEmployees(orderedList);
+
+        System.out.println();
+
+        BigDecimal salariesSum = funcionarios.stream().map(Funcionario::getSalario).reduce(BigDecimal.ZERO,
+                BigDecimal::add);
+
+        System.out.println("3.11");
+        System.out.println(Funcionario.formatSalary(salariesSum) + "\n");
+
+        System.out.println(
+                "3.12");
+
+        for (Funcionario funcionario : funcionarios) {
+            System.out.println(funcionario.getNome() + " recebe "
+                    + funcionario.basicSalaries(minimumWage) + " salário(s) mínimo(s).");
+        }
     }
 
-    System.out.println();
+    public static List<Funcionario> removeEmployee(List<Funcionario> funcionarios, String nome) {
+        List<Funcionario> novaLista = new ArrayList<>(funcionarios);
+        novaLista.removeIf(s -> s.getNome().equals(nome));
 
-    // 3.5
-    Map<String, List<Funcionario>> sortByRole = funcionarios.stream()
-        .collect(Collectors.groupingBy(Funcionario::getFuncao));
+        System.out.println("3.2");
+        System.out.println(novaLista);
 
-    System.out.println();
-
-    System.out.println("3.6");
-    System.out.println(sortByRole.entrySet());
-
-    System.out.println();
-
-    System.out.println("3.8");
-    System.out
-        .println(
-            funcionarios.stream()
-                .filter(s -> s.getDataNascimento().getMonth().getValue() == 10
-                    || s.getDataNascimento().getMonth().getValue() == 12)
-                .collect(Collectors.toList()));
-
-    System.out.println();
-
-    System.out.println(
-        "3.9");
-
-    Comparator<Funcionario> comparator = (func1, func2) -> func1.getDataNascimento()
-        .compareTo(func2.getDataNascimento());
-
-    Collections.sort(funcionarios, comparator);
-
-    Funcionario oldest = funcionarios.get(0);
-    System.out
-        .println("Nome: " + oldest.getNome() + ", Idade: " + oldest.getAge() + " anos.");
-
-    System.out.println();
-
-    List<Funcionario> orderedList = funcionarios.stream()
-        .sorted((arg0, arg1) -> arg0.getNome().compareTo(arg1.getNome()))
-        .collect(Collectors.toList());
-
-    System.out.println("3.10");
-    printEmployees(orderedList);
-
-    System.out.println();
-
-    BigDecimal salariesSum = funcionarios.stream().map(Funcionario::getSalario).reduce(BigDecimal.ZERO,
-        BigDecimal::add);
-
-    System.out.println("3.11");
-    System.out.println(Funcionario.formatSalary(salariesSum) + "\n");
-
-    System.out.println(
-        "3.12");
-
-    for (Funcionario funcionario : funcionarios) {
-      System.out.println(funcionario.getNome() + " recebe "
-          + funcionario.basicSalaries(minimumWage) + " salário(s) mínimo(s).");
+        return novaLista;
     }
-  }
 
-  public static List<Funcionario> removeEmployee(List<Funcionario> funcionarios, String nome) {
-    List<Funcionario> novaLista = new ArrayList<>(funcionarios);
-    novaLista.removeIf(s -> s.getNome().equals(nome));
-
-    System.out.println("3.2");
-    System.out.println(novaLista);
-
-    return novaLista;
-  }
-
-  public static void printEmployees(List<Funcionario> funcionarios) {
-    System.out.println("3.3");
-    for (Funcionario funcionario : funcionarios) {
-      System.out.println(funcionario);
+    public static void printEmployees(List<Funcionario> funcionarios) {
+        System.out.println("3.3");
+        for (Funcionario funcionario : funcionarios) {
+            System.out.println(funcionario);
+        }
     }
-  }
 }
